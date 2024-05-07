0a96c8f9709df405820c5f6411056718c15b7cf5
HamzaGbada <hamza.gbada@gmail.com>
2024-05-07 14:19:01 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonitorComponent } from './monitor/monitor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonitorComponent
   ],
   imports: [
     BrowserModule,
