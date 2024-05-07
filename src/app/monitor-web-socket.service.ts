36a9905deb6f76a085aa8f799241ebac44808692
HamzaGbada <hamza.gbada@gmail.com>
2024-05-07 14:28:43 +0100

    [ADD] websocket

--- src/app/monitor-web-socket.service.ts
+++ src/app/monitor-web-socket.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Client, Message } from '@stomp/stompjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MonitorWebSocketService {
+  private stompClient: Client;
 
-  constructor() { }
+  constructor() {
+    this.stompClient = new Client({
+      brokerURL: 'ws://localhost:8080/ws/kafka-topic',
+      reconnectDelay: 5000,
+      debug: (str: string) => {
+        console.log(str);
+      }
+    });
+    this.stompClient.activate();
+  }
+
+  // Method to subscribe to WebSocket messages
+  getMessage(callback: (message: Message) => void) {
+    this.stompClient.onConnect = () => {
+      this.stompClient.subscribe('/topic/kafka-topic', callback);
+    };
+  }
 }
--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message } from '@stomp/stompjs';
+import { MonitorWebSocketService } from '../monitor-web-socket.service';
 
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.component.html',
-  styleUrl: './monitor.component.css'
+  styleUrls: ['./monitor.component.css']
 })
-export class MonitorComponent {
+export class MonitorComponent implements OnInit {
+  messages: any[] = [];
 
+  constructor(private monitorWebSocketService: MonitorWebSocketService) { }
+
+  ngOnInit(): void {
+    // Subscribe to WebSocket messages
+    this.monitorWebSocketService.getMessage((message: Message) => {
+      this.messages.push(JSON.parse(message.body));
+    });
+  }
 }
