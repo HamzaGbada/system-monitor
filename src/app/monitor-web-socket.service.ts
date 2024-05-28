a97da4edea5ba9daff3ce8340aa91fbe4347ef25
HamzaGbada <hamza.gbada@gmail.com>
2024-05-28 17:59:39 +0100

    [ADD] websocket

--- src/app/monitor-web-socket.service.ts
+++ src/app/monitor-web-socket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BehaviorSubject} from "rxjs";
-import {Client, Stomp} from "@stomp/stompjs";
+import {Client} from "@stomp/stompjs";
+import * as Stomp from 'stompjs';
 
 
 @Injectable({
@@ -19,15 +20,23 @@ export class MonitorWebSocketService {
   }
 
   connect(): void {
-    this.stompClient.connect({}, frame => {
+    this.stompClient.connect({}, (frame: any) => {
       console.log('Connected: ' + frame);
-      this.stompClient.subscribe('/all/messages', greeting => {
-        console.log(JSON.parse(greeting.body).content);
-      });
-    }, error => {
+      this.subscribeToMessages();
+    }, (error: Error) => { // Explicitly specify the type as 'Error'
       console.error('Error with websocket', error);
+      // You may want to handle the error here, such as retrying the connection
     });
   }
+
+  private subscribeToMessages(): void {
+    this.stompClient.subscribe('/all/messages', (greeting: Stomp.Message) => {
+      const messageContent = JSON.parse(greeting.body).content;
+      console.log('Received message:', messageContent);
+      // Handle the message content as needed
+    });
+  }
+
 
   disconnect(): void {
     if (this.stompClient) {
